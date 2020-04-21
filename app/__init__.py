36006584605dffea6ceeef2075aa2eb84163fb07
Aditi-Billore <aditi.billore3@gmail.com>
2020-04-22 00:34:22 +0530

    GUI Changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_moment import Moment
 import os
 import logging
 from logging.handlers import SMTPHandler,RotatingFileHandler
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__,template_folder='../templates')
 app.config.from_object(Config)
@@ -13,6 +16,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+moment = Moment(app)
+bootstrap = Bootstrap(app)
 
 from app import routes,models, errors
 
